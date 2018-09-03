e4b94299e7365686e456723bd941b4ea776a77be
prateek76 <prateek7672168@gmail.com>
2018-09-03 15:52:20 +0530

    admin dashboard fetching for accepted,rejected,pending,all applications completed && backend checks for form completed

--- core/db.php
+++ core/db.php
@@ -1,18 +1,18 @@
 <?php
 #$host = "db4free.net:3306"; 
 
-//$dsn = 'mysql:host=localhost;dbname=leaveapp';
-$dsn = 'mysql:host=localhost;dbname=leave_portal2';
+$dsn = 'mysql:host=localhost;dbname=leaveapp';
+//$dsn = 'mysql:host=171.76.189.54:3306;dbname=leave_portal2';
 $username = 'root';
 $pass = '';
 
 
 try{
 	$connection = new PDO($dsn , $username ,$pass) ;
-
+	#$conn = new PDO('mysql:host=db4free.net;dbname=leave_portal2;port=3306','geekhaven2','Geekhaven@123');
 	
-}catch(PDOException $e){
-	die("no connection");
+}catch(Exception $e){
+	echo $e;
 }
 
 ?>
