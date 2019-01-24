6314ebc369db34844e12f8b24a9d1aa74d9ea17f
prateek <prateek7672168@gmail.com>
2019-01-25 00:14:45 +0530

    removed underline from new application button

--- student_dashboard.php
+++ student_dashboard.php
@@ -61,7 +61,10 @@
 
 		@media screen and (max-height: 450px) {
 			.sidenav {padding-top: 15px;}
-			.sidenav a {font-size: 18px;}
+			.sidenav a {
+				font-size: 18px;
+				text-decoration: none;		
+			}
 		}
 
 		input {
