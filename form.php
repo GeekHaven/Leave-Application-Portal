79076a41c5ad1979652207fe08b0907d639a543f
Manthan Surkar <42006277+thesmallstar@users.noreply.github.com>
2019-02-02 22:51:36 +0530

    Fixes #82 Start and end date (#84)
    * squash
    
    * Update admin-login-verify.php
    
    * Update admin_dashboard.php
    
    * squash
    
    * FIX
    
    * Logout fixes
    
    * Fixed
    
    * Update admin_dashboard.php
    
    * Update application-submission.php
    
    * Create x.png
    
    * Update form.php
    
    * Update form.css
    
    * Update application-submission.php
    
    * Update form.php

--- form.php
+++ form.php
@@ -280,5 +280,20 @@ $('body').on('change', '#file', function(){
         }
     });
 });
+
+
     </script>
+    
+    <script>
+$("#endDate").change(function () {
+    var startDate = document.getElementById("startDate").value;
+    var endDate = document.getElementById("endDate").value;
+
+    if ((Date.parse(startDate) >= Date.parse(endDate))) {
+        alert("End date should be greater than Start date");
+        document.getElementById("endDate").value = "";
+    }
+});
+
+</script>
 </body>
