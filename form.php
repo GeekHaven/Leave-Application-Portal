291de904a10dd0c9dd4064181be879760ad428f0
Prateek76 <iec2017029@iiita.ac.in>
2019-02-02 22:56:44 +0530

    minor fix

--- form.php
+++ form.php
@@ -289,7 +289,7 @@ $("#endDate").change(function () {
     var startDate = document.getElementById("startDate").value;
     var endDate = document.getElementById("endDate").value;
 
-    if ((Date.parse(startDate) >= Date.parse(endDate))) {
+    if ((Date.parse(startDate) > Date.parse(endDate))) {
         alert("End date should be greater than Start date");
         document.getElementById("endDate").value = "";
     }
